9771f125ccf97ea87ed9873678229d54847080fb
Vishal Kumar Rai <7376vishalkumarrai@gmail.com>
2021-03-28 17:01:36 +0530

    backend
    backend

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 5000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+let getroll = [];
+let result = {};
+
+app.post("/getresult", async (req, res) => {
+  let roll = req.body.roll;
+  getroll = roll.split(",");
+  // console.log(getroll);
+  await Promise.all(
+    getroll.map((e) =>
+      axios
+        .get(`https://terriblytinytales.com/testapi?rollnumber=${parseInt(e)}`)
+        .then((data) => {
+          return data.data;
+        })
+        .then((report) => {
+          result[e] = report;
+        })
+    )
+  );
+
+  // console.log(result);
+  res.send(result);
+  result = {};
+});
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
